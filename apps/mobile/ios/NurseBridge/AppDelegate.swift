a2aa9a8a983c62e183ac0cd2a07816be5cd2d519
MasterGKC <kossivigbleguede@MasterGKCs-MacBook-Pro.local>
2026-07-30 15:39:22 -0400

    fix(ios): load Debug JavaScript from Metro

--- apps/mobile/ios/NurseBridge/AppDelegate.swift
+++ apps/mobile/ios/NurseBridge/AppDelegate.swift
@@ -95,9 +95,6 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    if let embeddedBundleURL = Bundle.main.url(forResource: "main", withExtension: "jsbundle") {
-      return embeddedBundleURL
-    }
     return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
